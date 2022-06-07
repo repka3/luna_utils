b957cf93986b7462f6fd50c0b43e7d8bde1560ee
alessio <lunardelli.alessio@gmail.com>
2022-06-07 13:44:47 +0200

    0.02

---
+++ dist/io_utils.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.nonEmptyStringOrNull_caststr = exports.nonEmptyStringOrNull = exports.positiveIntOrNull_castNumber = void 0;
+function positiveIntOrNull_castNumber(input) {
+    if (input == null || input == undefined || isNaN(+input) || +input < 1)
+        return null;
+    return +input;
+}
+exports.positiveIntOrNull_castNumber = positiveIntOrNull_castNumber;
+function nonEmptyStringOrNull(ss, minlength = 1, maxlength = 255, allowLiteralNullInString = true) {
+    if (ss === null ||
+        ss === undefined ||
+        typeof ss != 'string' ||
+        ss.length < minlength ||
+        ss.length > maxlength) {
+        return null;
+    }
+    if (!allowLiteralNullInString && ss === 'null')
+        return null;
+    return ss;
+}
+exports.nonEmptyStringOrNull = nonEmptyStringOrNull;
+function nonEmptyStringOrNull_caststr(ss, minlength = 1, maxlength = 255, allowLiteralNullInString = true) {
+    if (ss === null || ss === undefined) {
+        return null;
+    }
+    const stringa = '' + ss;
+    if (stringa.length < minlength || stringa.length > maxlength)
+        return null;
+    if (!allowLiteralNullInString && stringa === 'null')
+        return null;
+    return stringa;
+}
+exports.nonEmptyStringOrNull_caststr = nonEmptyStringOrNull_caststr;
